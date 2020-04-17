fdb6db8619da95496d7882e9cd0360a3c1390dbb
Olli Juhola <juhola.olli@gmail.com>
2020-04-17 17:07:00 +0300

    Project commit

---
+++ Assets/Building System/Scripts/Placeable.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeable : MonoBehaviour
+{
+    [SerializeField] public int AfterBuildLayer = 4;
+
+    [Header("Build Time")]
+    [SerializeField] public float BuildTime = 2f;
+    [SerializeField] public GameObject ConstructionEffect;
+
+    [HideInInspector] public bool CanBePlaced = false;
+
+    bool BeingBuilt = false;
+
+    LayerMask PlacementLayer;
+    List<GameObject> CollisionList = new List<GameObject>();
+
+    List<CachedRenderer> Renderers = new List<CachedRenderer>();
+
+    private void Awake()
+    {
+        if (!GetComponent<Rigidbody>())
+        {
+            gameObject.AddComponent<Rigidbody>();
+        }
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<Rigidbody>().useGravity = false;
+
+        if (GetComponent<MeshRenderer>())
+        {
+            Renderers.Add(new CachedRenderer(GetComponent<MeshRenderer>(), GetComponent<MeshRenderer>().materials));
+        }
+
+        foreach (MeshRenderer rend in GetComponentsInChildren<MeshRenderer>())
+        {
+            Renderers.Add(new CachedRenderer(rend, rend.materials));
+        }
+
+        PlacementLayer = PlacementManager.Instance.DetectionLayer;
+    }
+
+    public Bounds GetBounds()
+    {
+        Bounds bounds = new Bounds(this.transform.position, Vector3.zero);
+
+        foreach (Collider item in GetComponentsInChildren<Collider>())
+        {
+            bounds.Encapsulate(item.bounds);
+        }
+
+        return bounds;
+    }
+
+    public void InitializeObjectPlacement(bool useBuildTime)
+    {
+        BeingBuilt = true;
+        SetLayer();
+
+        if (useBuildTime)
+        {
+            StartCoroutine(BuildObject());
+        }
+        else
+        {
+            PlaceObject();
+        }
+
+    }
+
+    IEnumerator BuildObject()
+    {
+        GameObject Effect = null;
+
+        if (ConstructionEffect != null)
+        {
+            Effect = Instantiate(ConstructionEffect, GetBounds().center, ConstructionEffect.transform.rotation, this.transform);
+        }
+
+        float passedTime = 0f;
+
+        while (passedTime <= BuildTime)
+        {
+            passedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        if (Effect != null)
+            Destroy(Effect);
+
+        PlaceObject();
+    }
+
+    void PlaceObject()
+    {
+        foreach (CachedRenderer rend in Renderers)
+        {
+            rend.Renderer.materials = rend.Materials;
+        }
+
+        Destroy(GetComponent<Rigidbody>());
+        Destroy(this);
+    }
+
+    public void SetMaterial(Material material)
+    {
+
+        foreach (CachedRenderer rend in Renderers)
+        {
+            Material[] mats = new Material[rend.Materials.Length];
+
+            for (int i = 0; i < rend.Materials.Length; i++)
+            {
+                mats[i] = material;
+            }
+
+            rend.Renderer.materials = mats;
+        }
+    }
+
+    public void SetLayer()
+    {
+        foreach (CachedRenderer go in Renderers)
+        {
+            go.Renderer.gameObject.layer = AfterBuildLayer;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (BeingBuilt)
+            return;
+
+        if (Input.GetKey(PlacementManager.Instance.RotateKey))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * PlacementManager.Instance.RotateSpeed);
+        }
+
+        if (CollisionList.Count > 0)
+        {
+            CanBePlaced = false;
+        }
+        else
+        {
+            CanBePlaced = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (PlacementLayer == (PlacementLayer | (1 << other.gameObject.layer)))
+        {
+            if (!CollisionList.Contains(other.gameObject))
+                CollisionList.Add(other.gameObject);
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (PlacementLayer == (PlacementLayer | (1 << other.gameObject.layer)))
+        {
+            CollisionList.Remove(other.gameObject);
+        }
+    }
+}
+
+[System.Serializable]
+public class CachedRenderer
+{
+    public MeshRenderer Renderer;
+    public Material[] Materials;
+
+    public CachedRenderer(MeshRenderer Rend, Material[] Mats)
+    {
+        Renderer = Rend;
+        Materials = Mats;
+    }
+}
---
+++ Assets/Building System/Scripts/PlacementManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementManager : Singleton<PlacementManager>
+{
+    [Header("Base")]
+    [SerializeField] private Placeable[] Placeables;
+    [SerializeField] private Camera Cam;
+
+    [Header("Layers")]
+    [SerializeField] LayerMask PlacementLayer;
+    [SerializeField] LayerMask detectionLayer;
+
+    [Header("Build Time")]
+    [SerializeField] private bool UseBuildTime = false;
+
+    [Header("Materials")]
+    [SerializeField] private Material PlaceableMat;
+    [SerializeField] private Material NotPlaceableMat;
+
+    [Header("Rotation")]
+    [SerializeField] private KeyCode rotateKey = KeyCode.R;
+    [SerializeField] private float rotateSpeed = 5f;
+
+    Placeable selectedPlaceable;
+    Placeable SelectedPlaceable
+    {
+        get { return selectedPlaceable; }
+        set
+        {
+            selectedPlaceable = value;
+        }
+    }
+
+    public LayerMask DetectionLayer { get => detectionLayer; }
+    public KeyCode RotateKey { get => rotateKey; }
+    public float RotateSpeed { get => rotateSpeed; set => rotateSpeed = value; }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (SelectedPlaceable == null)
+            return;
+
+        UpdateObjectPosition();
+    }
+
+    private void Update()
+    {
+        PickPlaceable();
+
+        if (SelectedPlaceable == null)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && SelectedPlaceable.CanBePlaced)
+            PlaceObject();
+
+    }
+
+    void PickPlaceable()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetPlaceable(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetPlaceable(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetPlaceable(2);
+        }
+    }
+
+    void SetPlaceable(int index)
+    {
+        if (SelectedPlaceable != null)
+            Destroy(SelectedPlaceable.gameObject);
+
+        SelectedPlaceable = Instantiate(Placeables[index].gameObject, Vector3.zero, Quaternion.identity, null).GetComponent<Placeable>();
+    }
+
+    void PlaceObject()
+    {
+        SelectedPlaceable.InitializeObjectPlacement(UseBuildTime);
+        SelectedPlaceable = null;
+    }
+
+    void UpdateObjectPosition()
+    {
+        Bounds bounds = SelectedPlaceable.GetBounds();
+
+        Ray cameraRay = Cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(cameraRay, out hit, Mathf.Infinity, PlacementLayer))
+        {
+            float yOffset = SelectedPlaceable.transform.position.y - bounds.min.y;
+            SelectedPlaceable.transform.position = Vector3.Slerp(SelectedPlaceable.transform.position, new Vector3(hit.point.x, hit.point.y + yOffset, hit.point.z), 1);
+        }
+
+        if (SelectedPlaceable.CanBePlaced)
+        {
+            SelectedPlaceable.SetMaterial(PlaceableMat);
+        }
+        else
+        {
+            SelectedPlaceable.SetMaterial(NotPlaceableMat);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        if (SelectedPlaceable != null)
+        {
+            Bounds bounds = SelectedPlaceable.GetBounds();
+            var center = bounds.center;
+            var size = bounds.size;
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireCube(center, new Vector3(size.x, size.y, size.z));
+        }
+    }
+}
